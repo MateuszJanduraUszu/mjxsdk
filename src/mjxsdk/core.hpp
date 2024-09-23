0c2580136f8fe0061669f85d5b000e160e477dda
MateuszJanduraUszu <mjandura03@gmail.com>
2024-09-23 11:54:18 +0200

    Set up compilers and toolchains

--- src/mjxsdk/core.hpp
+++ src/mjxsdk/core.hpp
@@ -9,7 +9,7 @@
 
 #ifdef _MJX_MSVC
 #define _MJX_NOVTABLE __declspec(novtable)
-#else // ^^^ _MJX_MSVC ^^^ / vvv !_MJX_MSVC vvv
+#else // ^^^ MSVC ^^^ / vvv Clang or GCC vvv
 #define _MJX_NOVTABLE
 #endif // _MJX_MSVC
 #endif // _MJXSDK_CORE_HPP_
