3134f0ffa2e65b714d75952d1886db6dce0ace41
Mateusz Jandura <mjandura03@gmail.com>
2025-01-26 00:43:44 +0100

    Implement `memory_resource` (#16)

---
+++ src/mjxsdk/memory_resource.cpp
@@ -0,0 +1,110 @@
+// memory_resource.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstring>
+#include <mjxsdk/impl/global_allocator.hpp>
+#include <mjxsdk/impl/utils.hpp>
+#include <mjxsdk/memory_resource.hpp>
+#include <utility>
+
+namespace mjx {
+    memory_resource::memory_resource() noexcept : _Myptr(nullptr), _Mysize(0) {}
+
+    memory_resource::memory_resource(const memory_resource& _Other) : _Myptr(nullptr), _Mysize(0) {
+        _Copy_resource(_Other._Myptr, _Other._Mysize);
+    }
+
+    memory_resource::memory_resource(memory_resource&& _Other) noexcept : _Myptr(nullptr), _Mysize(0) {
+        _Move_resource(_Other._Myptr, _Other._Mysize);
+    }
+
+    memory_resource::memory_resource(const size_type _Size)
+        : _Myptr(mjxsdk_impl::_Get_internal_allocator().allocate(_Size)), _Mysize(_Size) {}
+
+    memory_resource::~memory_resource() noexcept {
+        destroy();
+    }
+
+    memory_resource& memory_resource::operator=(const memory_resource& _Other) {
+        if (this != &_Other) {
+            destroy();
+            _Copy_resource(_Other._Myptr, _Other._Mysize);
+        }
+
+        return *this;
+    }
+
+    memory_resource& memory_resource::operator=(memory_resource&& _Other) noexcept {
+        if (this != &_Other) {
+            destroy();
+            _Move_resource(_Other._Myptr, _Other._Mysize);
+        }
+
+        return *this;
+    }
+
+    void memory_resource::_Copy_resource(const_pointer _Ptr, const size_type _Size) {
+        _Myptr  = mjxsdk_impl::_Get_internal_allocator().allocate(_Size);
+        _Mysize = _Size;
+        ::memcpy(_Myptr, _Ptr, _Size);
+    }
+
+    void memory_resource::_Move_resource(pointer& _Ptr, size_type& _Size) noexcept {
+        _Myptr  = _Ptr;
+        _Mysize = _Size;
+        _Ptr    = nullptr;
+        _Size   = 0;
+    }
+
+    bool memory_resource::empty() const noexcept {
+        return _Myptr == nullptr && _Mysize == 0;
+    }
+
+    memory_resource::pointer memory_resource::data() noexcept {
+        return _Myptr;
+    }
+
+    memory_resource::const_pointer memory_resource::data() const noexcept {
+        return _Myptr;
+    }
+
+    memory_resource::size_type memory_resource::size() const noexcept {
+        return _Mysize;
+    }
+
+    bool memory_resource::contains(const_pointer _Block, const size_type _Size) const noexcept {
+        if (empty() || !_Block || _Size == 0) {
+            return false;
+        }
+
+        const_pointer _Base_end  = mjxsdk_impl::_Adjust_address_by_offset(_Myptr, _Mysize);
+        const_pointer _Block_end = mjxsdk_impl::_Adjust_address_by_offset(_Block, _Size);
+        return mjxsdk_impl::_Is_within_memory_block(_Myptr, _Base_end, _Block, _Block_end);
+    }
+
+    void memory_resource::swap(memory_resource& _Other) noexcept {
+        ::std::swap(_Myptr, _Other._Myptr);
+        ::std::swap(_Mysize, _Other._Mysize);
+    }
+
+    memory_resource::release_result memory_resource::release() noexcept {
+        const release_result _Result = {_Myptr, _Mysize};
+        _Myptr                       = nullptr;
+        _Mysize                      = 0;
+        return _Result;
+    }
+
+    void memory_resource::destroy() noexcept {
+        if (_Myptr && _Mysize > 0) {
+            mjxsdk_impl::_Get_internal_allocator().deallocate(_Myptr, _Mysize);
+            _Myptr  = nullptr;
+            _Mysize = 0;
+        }
+    }
+
+    bool operator==(const memory_resource& _Left, const memory_resource& _Right) noexcept {
+        return _Left.data() == _Right.data() && _Left.size() == _Right.size();
+    }
+} // namespace mjx
---
+++ src/mjxsdk/memory_resource.hpp
@@ -0,0 +1,71 @@
+// memory_resource.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJXSDK_MEMORY_RESOURCE_HPP_
+#define _MJXSDK_MEMORY_RESOURCE_HPP_
+#include <mjxsdk/allocator.hpp>
+#include <mjxsdk/export.hpp>
+
+namespace mjx {
+    class _MJXSDK_EXPORT memory_resource { // memory block that serves as a resource for allocators
+    public:
+        using size_type       = allocator::size_type;
+        using difference_type = allocator::difference_type;
+        using pointer         = allocator::pointer;
+        using const_pointer   = const pointer;
+    
+        memory_resource() noexcept;
+        memory_resource(const memory_resource& _Other);
+        memory_resource(memory_resource&& _Other) noexcept;
+        ~memory_resource() noexcept;
+
+        explicit memory_resource(const size_type _Size);
+
+        memory_resource& operator=(const memory_resource& _Other);
+        memory_resource& operator=(memory_resource&& _Other) noexcept;
+        
+        // checks whether the resource is empty
+        bool empty() const noexcept;
+
+        // returns the stored data
+        pointer data() noexcept;
+        const_pointer data() const noexcept;
+
+        // returns the size of the resource
+        size_type size() const noexcept;
+        
+        // checks whether the specified memory block comes from this resource
+        bool contains(const_pointer _Block, const size_type _Size) const noexcept;
+
+        // swaps two resources
+        void swap(memory_resource& _Other) noexcept;
+
+        struct release_result {
+            pointer ptr;
+            size_type size;
+        };
+
+        // releases the resource
+        release_result release() noexcept;
+
+        // destroys the resource
+        void destroy() noexcept;
+
+    private:
+        // copies other resource
+        void _Copy_resource(const_pointer _Ptr, const size_type _Size);
+
+        // moves other resource
+        void _Move_resource(pointer& _Ptr, size_type& _Size) noexcept;
+
+        pointer _Myptr;
+        size_type _Mysize;
+    };
+
+    _MJXSDK_EXPORT bool operator==(const memory_resource& _Left, const memory_resource& _Right) noexcept;
+} // namespace mjx
+
+#endif // _MJXSDK_MEMORY_RESOURCE_HPP_
